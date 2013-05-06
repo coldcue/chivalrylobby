cd38a8630f475a6fbe906b626332960db99eaf63
coldcue <coldcue@gmail.com>
2013-05-06 19:12:04 +0200

    test things added

--- chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/IndexController.java
+++ chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/IndexController.java
@@ -2,14 +2,14 @@ package com.chivalrylobby.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping("/index")
-	public @ResponseBody
-	String index() {
-		return "Hi!";
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("index/index");
+		return mav;
 	}
 }
