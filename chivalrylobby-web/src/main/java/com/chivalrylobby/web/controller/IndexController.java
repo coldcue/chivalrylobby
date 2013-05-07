e1bbcc9f62d652e67befb22082168a7232616437
coldcue <coldcue@gmail.com>
2013-05-07 16:39:36 +0200

    now its working

--- chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/IndexController.java
+++ chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/IndexController.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.transaction.Transaction;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
