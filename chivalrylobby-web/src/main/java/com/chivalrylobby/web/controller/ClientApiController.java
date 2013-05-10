6456f4cb7119a5ba73e75e2a6dde66b071459c43
coldcue <coldcue@gmail.com>
2013-05-10 19:43:38 +0200

    some logging added

--- chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/ClientApiController.java
+++ chivalrylobby-web/src/main/java/com/chivalrylobby/web/controller/ClientApiController.java
@@ -1,6 +1,8 @@
 package com.chivalrylobby.web.controller;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.chivalrylobby.web.entity.Server;
 import com.chivalrylobby.web.json.support.RegisterServer;
 import com.chivalrylobby.web.json.support.ResponseMessage;
 import com.chivalrylobby.web.service.ServersService;
@@ -17,13 +20,15 @@ import com.chivalrylobby.web.service.ServersService;
 @Controller
 @RequestMapping("/clapi")
 public class ClientApiController {
+	private static final Logger log = Logger
+			.getLogger(ClientApiController.class.getName());
 
 	@Autowired
 	ServersService serversService;
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public @ResponseBody
-	ResponseMessage clapi(
+	ResponseMessage register(
 			@RequestParam(required = true, value = "q") String query)
 			throws Exception {
 
@@ -31,16 +36,15 @@ public class ClientApiController {
 			ObjectMapper mapper = new ObjectMapper();
 			RegisterServer registerServer = mapper.readValue(query,
 					RegisterServer.class);
-			serversService.registerServer(registerServer);
+			Server server = serversService.registerServer(registerServer);
+			log.info("New server registered! id:" + server.getKey().getId());
 		} catch (IOException e) {
-			throw e;
-			// return new ResponseMessage(false,
-			// "Are you kidding me? bad format... try harder!");
+			log.log(Level.WARNING, "Wrongly formatted registering: " + query, e);
+			return new ResponseMessage(false,
+					"Are you kidding me? bad format... try harder!");
 		} catch (Exception e) {
-			throw e;
-			// return new ResponseMessage(false, "Server registration failed!");
+			return new ResponseMessage(false, "Server registration failed!");
 		}
-
 		return new ResponseMessage(true, "Server successfully registered!");
 	}
 }
